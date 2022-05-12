093945133a8020cd55e2efa731bb772fb71f7d5f
wuyushuai <970443232@qq.com>
2022-05-12 08:45:06 +0800

    a

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -10,5 +15,11 @@ func main() {
 		})
 
 	})
+	r.POST("/msg", msg)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func msg(c *gin.Context) {
+	jsonData, _ := ioutil.ReadAll(c.Request.Body)
+	fmt.Println(string(jsonData))
 }
